a51bd3863d09536505bbbddc48f5e9a0478293ee
GonnenDaube <gonen.daube@gmail.com>
2017-12-10 19:48:21 +0200

    added color picker

--- WebProject-Site/Pages/WorkingSpace/WorkingSpace.aspx
+++ WebProject-Site/Pages/WorkingSpace/WorkingSpace.aspx
@@ -4,32 +4,47 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="body" Runat="Server">
     <div class="left-btn-coll">
-        <button id="triangle-mode" class="btn-left">
+        <button id="triangle-mode" class="btn-left checkable-left">
             <img id="triangle-icon" src="/Resources/Icons/triangle_icon.png" class="btn-left-icon"/>
         </button>
-        <button id="triangle-strip-mode" class="btn-left">
+        <button id="triangle-strip-mode" class="btn-left checkable-left">
             <img id="triangle-strip-icon" src="/Resources/Icons/triangle_strip_icon.png" class="btn-left-icon"/>
         </button>
-        <button id="line-mode" class="btn-left">
+        <button id="line-mode" class="btn-left checkable-left">
             <img id="line-icon" src="/Resources/Icons/line_icon.png" class="btn-left-icon"/>
         </button>
-        <button id="line-strip-mode" class="btn-left">
+        <button id="line-strip-mode" class="btn-left checkable-left">
             <img id="line-strip-icon" src="/Resources/Icons/line_strip_icon.png" class="btn-left-icon"/>
         </button>
         <button class="btn-left"></button>
     </div>
     <div class="right-btn-coll">
-        <button id="wireframe-mode" class="btn-right">
+        <button id="wireframe-mode" class="btn-right checkable-right">
             <img id="wireframe-icon" src="/Resources/Icons/wireframe_icon.png" class="btn-right-icon"/>
         </button>
-        <button id="solid-mode" class="btn-right">
+        <button id="solid-mode" class="btn-right checkable-right">
             <img id="solid-icon" src="/Resources/Icons/solid_icon.png" class="btn-right-icon"/>
         </button>
-        <button id="lighting-mode" class="btn-right">
+        <button id="lighting-mode" class="btn-right checkable-right">
             <img id="lighting-icon" src="/Resources/Icons/phong_lighting_icon.png" class="btn-right-icon"/>
         </button>
-        <button class="btn-right"></button>
-        <button class="btn-right"></button>
+        <div id="color-picker" class="btn-right color-picker">
+            <div id="bars" class="bars">
+                <div id="color" class="bar" style="background:linear-gradient(rgb(255, 0, 0), rgb(255, 255, 0), rgb(0,255,0), rgb(0, 255, 255), rgb(0,0,255), rgb(255, 0, 255), rgb(255, 0, 0));" onmousemove="moveColorSelector();">
+                    <div id="colorSelector" class="selector" style="position:absolute; left:0%; top:-1%" onmousedown="enableColorSelectorPositionChange();" onmouseup="disableColorSelectorPositionChange();">
+                    </div>
+                </div>
+                <div id="saturation" class="bar" style="background:linear-gradient(rgb(255, 0, 0), rgb(255,255,255));" onmousemove="moveSaturSelector();">
+                    <div id="saturSelector" class="selector" style="position:absolute; left:0%; top:-1%" onmousedown="enableSaturSelectorPositionChange();" onmouseup="disableSaturSelectorPositionChange();">
+                    </div>
+                </div>
+                <div id="brightness" class="bar" style="background:linear-gradient(rgb(255, 0, 0), rgb(0,255,0), rgb(0,0,255));" onmousemove="moveBrightSelector();">
+                    <div id="brightSelector" class="selector" style="position:absolute; left:0%; top:-1%" onmousedown="enableBrightSelectorPositionChange();" onmouseup="disableBrightSelectorPositionChange();">
+                    </div>
+                </div>
+            </div>
+            <div id="final-color" class="final-color" style="background-color:rgb(255,0,0)"></div>
+        </div>
     </div>
     <div id="canvas-view" class="canvas-div">
         <canvas id="canvas" class="canvas">
---
+++ WebProject-Site/Pages/WorkingSpace/colorpicker.js
@@ -0,0 +1,191 @@
+﻿var colorSelector;
+var saturSelector;
+var brightSelector;
+var colorBar;
+var saturBar;
+var brightBar;
+var finalColor;
+
+var colorEnabled = false;
+var saturEnabled = false;
+var brightEnabled = false;
+
+function enableColorSelectorPositionChange() {
+    colorEnabled = true;
+}
+
+function enableSaturSelectorPositionChange() {
+    saturEnabled = true;
+}
+
+function enableBrightSelectorPositionChange() {
+    brightEnabled = true;
+}
+
+function disableColorSelectorPositionChange() {
+    colorEnabled = false;
+}
+
+function disableSaturSelectorPositionChange() {
+    saturEnabled = false;
+}
+
+function disableBrightSelectorPositionChange() {
+    brightEnabled = false;
+}
+
+function moveColorSelector() {
+    if (colorEnabled) {
+        var colorPosY = colorBar.getBoundingClientRect().top;
+        var colorHeight = colorBar.getBoundingClientRect().bottom - colorPosY;
+
+        var selectorNewPos = 100 * (event.clientY - colorPosY) / colorHeight;
+        if (selectorNewPos > 100)
+            selectorNewPos = 100;
+        if (selectorNewPos < 0)
+            selectorNewPos = 0;
+
+        colorSelector.style.top = selectorNewPos - 1 + "%";
+
+        updateColor();
+    }
+}
+
+function moveSaturSelector() {
+    if (saturEnabled) {
+        var colorPosY = saturBar.getBoundingClientRect().top;
+        var colorHeight = saturBar.getBoundingClientRect().bottom - colorPosY;
+
+        var selectorNewPos = 100 * (event.clientY - colorPosY) / colorHeight;
+        if (selectorNewPos > 100)
+            selectorNewPos = 100;
+        if (selectorNewPos < 0)
+            selectorNewPos = 0;
+
+        saturSelector.style.top = selectorNewPos - 1 + "%";
+
+        updateColor();
+    }
+}
+
+function moveBrightSelector() {
+    if (brightEnabled) {
+        var colorPosY = brightBar.getBoundingClientRect().top;
+        var colorHeight = brightBar.getBoundingClientRect().bottom - colorPosY;
+
+        var selectorNewPos = 100 * (event.clientY - colorPosY) / colorHeight;
+        if (selectorNewPos > 100)
+            selectorNewPos = 100;
+        if (selectorNewPos < 0)
+            selectorNewPos = 0;
+
+        brightSelector.style.top = selectorNewPos - 1 + "%";
+
+        updateColor();
+    }
+}
+
+if (window.location.href.includes("WorkingSpace.aspx")) {
+    addLoadEvent(colorPickerLoad);
+}
+
+function colorPickerLoad() {
+    colorBar = document.getElementById("color");
+    saturBar = document.getElementById("saturation");
+    brightBar = document.getElementById("brightness");
+
+    colorSelector = colorBar.children[0];
+    saturSelector = saturBar.children[0];
+    brightSelector = brightBar.children[0];
+    
+    finalColor = document.getElementById("final-color");
+
+    updateColor();
+}
+
+function updateColor() {
+
+    //color
+    let rgbPercent = colorSelector.style.top;
+    let rgbNum = parseInt(rgbPercent.substring(0, rgbPercent.length - 1)) + 1;//offset to zero
+
+    let red = 0;
+    let green = 0;
+    let blue = 0;
+
+    if (rgbNum <= 16.66) {
+        red = 255;
+        green = 255 * rgbNum / 16.66;
+        blue = 0;
+    }
+    else if (rgbNum <= 33.33) {
+        green = 255;
+        red = 255 * (33.33 - rgbNum) / 16.66;
+        blue = 0;
+    }
+    else if (rgbNum <= 50) {
+        red = 0;
+        blue = 255 * (rgbNum - 33.33) / 16.66;
+        green = 255;
+    }
+    else if (rgbNum <= 66.66) {
+        red = 0;
+        blue = 255;
+        green = 255 * (66.66 - rgbNum) / 16.66;
+    }
+    else if (rgbNum <= 83.32) {
+        red = 255 * (rgbNum - 66.66) / 16.66;
+        blue = 255;
+        green = 0;
+    }
+    else {
+        red = 255;
+        blue = 255 * (100 - rgbNum) / 16.66;
+        green = 0;
+    }
+
+    //saturation
+    saturBar.setAttribute("style", "background:linear-gradient(rgb(" + Math.round(red) + "," + Math.round(green) + "," + Math.round(blue) + "), rgb(255,255,255));");
+
+    let saturPercent = saturSelector.style.top;
+    let saturNum = parseInt(saturPercent.substring(0, saturPercent.length - 1)) + 1;//offset to zero
+    saturNum = 100 - saturNum;
+    saturNum = saturNum / 100;
+
+    var p = Math.sqrt(red * red * 0.299 + green * green * 0.587 + blue * blue * 0.114);
+
+    red = 2 * p + (red - 2 * p) * saturNum;
+    green = 2 * p + (green - 2 * p) * saturNum;
+    blue = 2 * p + (blue - 2 * p) * saturNum;
+
+    //brightness
+    brightBar.setAttribute("style", "background:linear-gradient(rgb(" + Math.round(red) + "," + Math.round(green) + "," + Math.round(blue) + "), rgb(0,0,0));");
+
+    let brightPercent = brightSelector.style.top;
+    let brightNum = parseInt(brightPercent.substring(0, brightPercent.length - 1)) + 1;//offset to zero
+    brightNum = 100 - brightNum;
+    brightNum = brightNum / 100;
+
+    red = brightNum * red;
+    green = brightNum * green;
+    blue = brightNum * blue;
+
+    //update color
+    finalColor.setAttribute("style", "background-color:rgb(" + Math.round(red) + "," + Math.round(green) + "," + Math.round(blue) + ");");
+}
+
+
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    }
+    else {
+        window.onload = function () {
+            if (oldonload) {
+                oldonload();
+            }
+            func();
+        }
+    }
+}
